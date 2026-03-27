f973cd63e5f0a9dad61f7eec33e75848d3be7552
ceboob <ceboolax@gmail.com>
2026-03-27 03:20:52 +0100

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Krzemienna Chata",
+  description: "Dom w lesie pod Supraślem"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pl">
+      <body>{children}</body>
+    </html>
+  );
+}
